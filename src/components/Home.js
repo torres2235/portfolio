b0bf3f9e211d95ea5743ae9f3d62536b99b831bb
torres2235 <43655957+torres2235@users.noreply.github.com>
2025-04-16 14:46:12 -0700

    draggable modal progress

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,23 +1,20 @@
-import React from "react";
+//import { useRef } from "react";
 import { useGlobalContext } from "../context/context";
-import Draggable from "react-draggable";
 
 const Home = () => {
   const { openModal } = useGlobalContext();
 
   return (
-    <main>
-      <Draggable>
-        <div>
-          <h1>Home Page</h1>
-          <p>Welcome :)</p>
-        </div>
-      </Draggable>
+    <>
+      <div>
+        <h1>Home Page</h1>
+        <p>Welcome :)</p>
+      </div>
 
       <button className="btn" onClick={openModal}>
         show modal
       </button>
-    </main>
+    </>
   );
 };
 
