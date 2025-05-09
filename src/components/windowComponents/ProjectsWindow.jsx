eefe221499a470311e0ce09b32d711329a2298ee
torres2235 <43655957+torres2235@users.noreply.github.com>
2025-05-08 18:57:24 -0700

    adding Project content

--- src/components/windowComponents/ProjectsWindow.jsx
+++ src/components/windowComponents/ProjectsWindow.jsx
@@ -4,6 +4,7 @@ import Draggable from "react-draggable";
 import { FaFolderOpen } from "react-icons/fa";
 
 import Window from "../Window";
+import boulderBuddy from "../../assets/boulderbuddy/image.png";
 
 const ProjectsWindow = ({ zIndex, parentClickHandler }) => {
   const { isProjectsOpen, closeProjects } = useGlobalContext();
@@ -33,56 +34,96 @@ const ProjectsWindow = ({ zIndex, parentClickHandler }) => {
           icon={<FaFolderOpen className="ml-1" />}
           windowName={"Projects"}
         >
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto
-            placeat unde dolorem vero, tenetur animi, reiciendis provident sequi
-            necessitatibus laborum quas sapiente ab odio nulla a delectus.
-            Eligendi, iusto. Et, quidem inventore fuga similique nisi corrupti
-            accusamus delectus eligendi sed repellat alias cum excepturi veniam
-            harum sequi corporis atque, nemo, deserunt quibusdam doloremque.
-            Consequatur nemo fuga error, ex eos quae ad et, reiciendis,
-            reprehenderit nam harum deserunt consectetur! Sint dolor accusantium
-            repellendus veritatis aperiam fuga magni corporis tenetur et magnam,
-            alias exercitationem explicabo culpa, eius nulla aliquid nesciunt
-            atque quidem dignissimos blanditiis repellat quasi est saepe? Earum
-            debitis magnam reiciendis velit quia suscipit distinctio aut,
-            quibusdam ut maiores nesciunt fugiat aspernatur saepe architecto
-            facere molestiae voluptatibus sed unde ex! Iure, sequi? Suscipit
-            velit tempore doloremque, aspernatur vel necessitatibus. Sunt rerum
-            id deserunt tenetur. Nemo pariatur quos aliquid maiores iste
-            praesentium. Aliquam blanditiis odio ex praesentium ut sapiente,
-            perferendis neque quae et voluptas excepturi ipsum modi, ea, iusto
-            libero? Ratione placeat aut sequi praesentium ipsam repudiandae
-            iure, eveniet recusandae consequuntur aspernatur pariatur magni
-            dicta veniam modi maxime aliquid alias quidem dolor nihil nesciunt
-            molestias nemo eius architecto id? Nulla rerum repellendus adipisci
-            cumque autem dolore libero mollitia, suscipit inventore vel in!
-          </p>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto
-            placeat unde dolorem vero, tenetur animi, reiciendis provident sequi
-            necessitatibus laborum quas sapiente ab odio nulla a delectus.
-            Eligendi, iusto. Et, quidem inventore fuga similique nisi corrupti
-            accusamus delectus eligendi sed repellat alias cum excepturi veniam
-            harum sequi corporis atque, nemo, deserunt quibusdam doloremque.
-            Consequatur nemo fuga error, ex eos quae ad et, reiciendis,
-            reprehenderit nam harum deserunt consectetur! Sint dolor accusantium
-            repellendus veritatis aperiam fuga magni corporis tenetur et magnam,
-            alias exercitationem explicabo culpa, eius nulla aliquid nesciunt
-            atque quidem dignissimos blanditiis repellat quasi est saepe? Earum
-            debitis magnam reiciendis velit quia suscipit distinctio aut,
-            quibusdam ut maiores nesciunt fugiat aspernatur saepe architecto
-            facere molestiae voluptatibus sed unde ex! Iure, sequi? Suscipit
-            velit tempore doloremque, aspernatur vel necessitatibus. Sunt rerum
-            id deserunt tenetur. Nemo pariatur quos aliquid maiores iste
-            praesentium. Aliquam blanditiis odio ex praesentium ut sapiente,
-            perferendis neque quae et voluptas excepturi ipsum modi, ea, iusto
-            libero? Ratione placeat aut sequi praesentium ipsam repudiandae
-            iure, eveniet recusandae consequuntur aspernatur pariatur magni
-            dicta veniam modi maxime aliquid alias quidem dolor nihil nesciunt
-            molestias nemo eius architecto id? Nulla rerum repellendus adipisci
-            cumque autem dolore libero mollitia, suscipit inventore vel in!
-          </p>
+          <div className="h-fit overflow-auto">
+            <div className="flex items-center gap-10 pb-4">
+              <div className="flex flex-row">
+                <img
+                  src={boulderBuddy}
+                  alt="BoulderBuddy"
+                  className="h-100 object-scale-down"
+                />
+                <h1 className="text-5xl text-blue-400">BoulderBuddy</h1>
+                <ul className="list-disc pl-8">
+                  <li>
+                    Mobile application to help climbers log track their climbs
+                  </li>
+                  <li>
+                    Designed and developed for cross‑platform usability using
+                    the React Native and Expo frameworks
+                  </li>
+                  <li>
+                    Integrated Expo Camera API to enable real‑time image capture
+                    within the app
+                  </li>
+                  <li>
+                    Implemented Camera Roll (Media Library API) for saving and
+                    retrieving user media files
+                  </li>
+                  <li>
+                    Managed permissions and optimized performance for seamless
+                    camera functionality across iOS and Android
+                  </li>
+                  <li>Uses AsyncStorage to hold our climb data locally •</li>
+                  <li>
+                    Created a Wireframe and User Flow Diagram to ensure a
+                    pleasant and streamlined UI/UX
+                  </li>
+                </ul>
+                <a
+                  href="https://github.com/torres2235/BoulderBuddy?tab=readme-ov-file"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="text-blue-400 hover:text-blue-200 hover:underline"
+                >
+                  Link to GitHub
+                </a>
+              </div>
+            </div>
+            <div className="flex items-center gap-10 pb-4 bg-gray-600 pb-4">
+              <div className="flex flex-row">
+                <img
+                  src={boulderBuddy}
+                  alt="BoulderBuddy"
+                  className="h-100 object-scale-down"
+                />
+                <h1 className="text-5xl text-blue-400">
+                  Billboard Top 50 to Spotify
+                </h1>
+                <ul className="list-disc pl-8">
+                  <li>
+                    Takes the selected year's top 50 songs from Billboard and
+                    creates a playlist on the user's Spotify
+                  </li>
+                  <li>
+                    Uses the Beautiful Soup Library to scrape Billboard's Top
+                    Songs for a given year
+                  </li>
+                  <li>
+                    Uses the Spotify API to access a user's Spotify and create
+                    the playlist within their account
+                  </li>
+                  <li>Developed using Python</li>
+                </ul>
+                <a
+                  href="https://github.com/torres2235/billboard-to-spotify"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="text-blue-400 hover:text-blue-200 hover:underline"
+                >
+                  Link to GitHub
+                </a>
+              </div>
+            </div>
+            <div>
+              <div>
+                <h3>And this website!</h3>
+                <p>
+                  A little cheesy I know, but I'm proud of it and plan on
+                  continuely add/improve on to it
+                </p>
+              </div>
+            </div>
+          </div>
         </Window>
       </div>
     </Draggable>
