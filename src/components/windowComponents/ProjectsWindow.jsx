eb40a7c7e4ca634e75ceb4a49acd3cbd7cc9f3aa
torres2235 <43655957+torres2235@users.noreply.github.com>
2025-05-11 01:01:03 -0700

    updating projects window

--- src/components/windowComponents/ProjectsWindow.jsx
+++ src/components/windowComponents/ProjectsWindow.jsx
@@ -4,7 +4,9 @@ import Draggable from "react-draggable";
 import { FaFolderOpen } from "react-icons/fa";
 
 import Window from "../Window";
-import boulderBuddy from "../../assets/boulderbuddy/image.png";
+import boulderBuddy from "../../assets/projectImages/boulderbuddy.png";
+import spotify from "../../assets/projectImages/spotify.png";
+import typing from "../../assets/projectImages/typing.jpg";
 
 const ProjectsWindow = ({ zIndex, parentClickHandler }) => {
   const { closeProjects, isProjectsVisible, toggleProjectsVisible } =
@@ -35,7 +37,7 @@ const ProjectsWindow = ({ zIndex, parentClickHandler }) => {
           windowName={"Projects"}
         >
           <div className="h-fit overflow-auto">
-            <div className="flex items-center gap-10 pb-4">
+            <div className="flex items-center gap-10 pb-4 pt-4">
               <div className="flex flex-row">
                 <img
                   src={boulderBuddy}
@@ -79,12 +81,40 @@ const ProjectsWindow = ({ zIndex, parentClickHandler }) => {
                 </a>
               </div>
             </div>
-            <div className="flex items-center gap-10 pb-4 bg-gray-600">
+            <div className="flex items-center gap-10 pb-4 pt-4 bg-gray-600">
               <div className="flex flex-row">
                 <img
-                  src={boulderBuddy}
-                  alt="BoulderBuddy"
-                  className="h-100 object-scale-down"
+                  src={typing}
+                  alt="typing"
+                  className="h-50 object-scale-down rounded-xl"
+                />
+                <h1 className="text-5xl text-blue-400">Typing Speed Test</h1>
+                <ul className="list-disc pl-8">
+                  <li>
+                    A Python GUI application made to test your typing speed
+                  </li>
+                  <li>
+                    Speed and accuracy are both measured in WPM and percentage
+                    respectively
+                  </li>
+                  <li>GUI developed using the TKinter library</li>
+                </ul>
+                <a
+                  href="https://github.com/torres2235/typing_speed_test"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="text-blue-400 hover:text-blue-200 hover:underline"
+                >
+                  Link to GitHub
+                </a>
+              </div>
+            </div>
+            <div className="flex items-center gap-10 pb-4 pt-4 ">
+              <div className="flex flex-row">
+                <img
+                  src={spotify}
+                  alt="spotify"
+                  className="h-50 object-scale-down"
                 />
                 <h1 className="text-5xl text-blue-400">
                   Billboard Top 50 to Spotify
@@ -114,14 +144,13 @@ const ProjectsWindow = ({ zIndex, parentClickHandler }) => {
                 </a>
               </div>
             </div>
-            <div>
-              <div>
-                <h3>And this website!</h3>
-                <p>
-                  A little cheesy I know, but I'm proud of it and am planning on
-                  continuely add/improve on to it
-                </p>
-              </div>
+
+            <div className="text-center pb-4 pt-4 bg-gray-600">
+              <h3>And this website!</h3>
+              <p>
+                A little cheesy I know, but I'm proud of it and am planning on
+                continuely add/improve on to it
+              </p>
             </div>
           </div>
         </Window>
